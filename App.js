d22d32cd8a9c4ef4c33ee229b9c4707e70d6e1e6
tlaudahl <travis.laudahl@gmail.com>
2019-12-13 07:43:34 -0600

    Added a modal for adding new goals

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { useState, useRef } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([])
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
   const flatListRef = useRef();
 
   const addGoalHandler = (goalTitle) => {
     setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle }]);
+    setIsAddMode(false);
   }
 
   const removeGoalHandler = goalId => {
@@ -18,10 +20,15 @@ export default function App() {
     })
   }
 
+  const cancelGoal = () => {
+    setIsAddMode(false);
+  }
+
 
   return (
     <View style={ styles.screen }>
-      <GoalInput addGoalHandler={addGoalHandler} />
+      <Button title='Add New Goal' onPress={() => setIsAddMode(true)} />
+      <GoalInput cancelGoal={cancelGoal} isAddMode={isAddMode} addGoalHandler={addGoalHandler} />
       <FlatList ref={flatListRef} onContentSizeChange={() => flatListRef.current.scrollToEnd()} data={courseGoals} renderItem={itemData => <GoalItem id={itemData.item.key} onDelete={removeGoalHandler} title={itemData.item.value} /> } />
     </View>
   );
