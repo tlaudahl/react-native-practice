db5395db720f4668def62066b28fffad95ea796b
tlaudahl <travis.laudahl@gmail.com>
2019-12-13 07:23:42 -0600

    Remove goal item functionality added

--- App.js
+++ App.js
@@ -12,11 +12,17 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle }]);
   }
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.key !== goalId)
+    })
+  }
+
 
   return (
     <View style={ styles.screen }>
       <GoalInput addGoalHandler={addGoalHandler} />
-      <FlatList ref={flatListRef} onContentSizeChange={() => flatListRef.current.scrollToEnd()} data={courseGoals} renderItem={itemData => <GoalItem onDelete={() => console.log('clicked')} title={itemData.item.value} /> } />
+      <FlatList ref={flatListRef} onContentSizeChange={() => flatListRef.current.scrollToEnd()} data={courseGoals} renderItem={itemData => <GoalItem id={itemData.item.key} onDelete={removeGoalHandler} title={itemData.item.value} /> } />
     </View>
   );
 }
