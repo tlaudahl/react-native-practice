3d7fbc33135c3f088ee89a85e665f3bf24e84216
tlaudahl <travis.laudahl@gmail.com>
2019-12-16 18:06:19 -0600

    Implemented a login page (doesn't actually login) that navigates to the GoalPage when the button is clicked using react-navigation/stack

--- App.js
+++ App.js
@@ -1,43 +1,23 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
-import moment from 'moment';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+import React from 'react';
+import GoalPage from './components/GoalPage';
+import Login from './components/Login';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
-export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
-  const flatListRef = useRef();
-
-  const now = moment();
-
-  const addGoalHandler = (goalTitle) => {
-    setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle, added: now.format('LLL') }]);
-    setIsAddMode(false);
-  }
-
-  const removeGoalHandler = goalId => {
-    setCourseGoals(currentGoals => {
-      return currentGoals.filter(goal => goal.key !== goalId)
-    })
-  }
-
-  const cancelGoal = () => {
-    setIsAddMode(false);
-  }
-
-
-  return (
-    <View style={ styles.screen }>
-      <Button title='Add New Goal' onPress={() => setIsAddMode(true)} />
-      <GoalInput cancelGoal={cancelGoal} isAddMode={isAddMode} addGoalHandler={addGoalHandler} />
-      <FlatList ref={flatListRef} onContentSizeChange={() => flatListRef.current.scrollToEnd()} data={courseGoals} renderItem={itemData => <GoalItem id={itemData.item.key} onDelete={removeGoalHandler} title={itemData.item.value} added={itemData.item.added} /> } />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  screen: {
-    padding: 50
+const AppNavigator = createStackNavigator(
+  {
+    Login: {
+      screen: Login
+    },
+    GoalPage: {
+      screen: GoalPage
+    }
   },
-});
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    }
+  })
+
+export default createAppContainer(AppNavigator);
